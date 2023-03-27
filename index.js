88049b8c1df7b5d661795fa3cdad6a4ea7df12c8
luciernaga3278 <126628801+luciernaga3278@users.noreply.github.com>
2023-03-27 19:57:35 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+
+
+let hidetextbtn =document.getElementById('hidetextbtn');
+
+let hidetext=document.getElementById('hidetext');
+
+hidetextbtn.addEventListener('click',toggleText);
+
+function toggleText() {
+
+    hidetext.classList.toggle('show');
+
+
+}
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
